7b5f34cb0e2b5bc206ceb037c7d6f36526f9af46
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-01-15 20:29:52 +0530

    progressbar

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,22 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by saeem  on 15/01/22.
+//
+//  The convention is to name your file same as the structure so we have named our files Question!
+
+import Foundation
+
+struct Question {    // Creating a structure to display different quesitons to the userm
+    
+    let text: String
+    let answer: String
+    
+    // Below we have created an initializer to reduce the code length of quiz array in View Controller
+    init(q:String, a:String) {
+        text=q
+        answer=a
+    }
+}
+
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,9 +15,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let quiz = [ // Created an array of question numbers
-       ["Four + Two is equal to Six.","True"],
-       ["Five - Three is greater than One","True"],
-       ["Three + Eight is less than Ten","False"]
+        Question(q: "A slug's blood is green.", a: "True"),
+                Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+                Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+                Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+                Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+                Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+                Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+                Question(q: "Google was originally called 'Backrub'.", a: "True"),
+                Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+                Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+                Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+                Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+       
+       // Above q & a came here from initializer that we created in question file
     ]
     
     var questionNumber = 0 // to keep track which question user is currently reading  so we have created this var questionnumber its value is 0 to match the arrow 0th index. which is the first question.
@@ -34,13 +45,13 @@ class ViewController: UIViewController {
         // Created a new constant
         let userAnswer = sender.currentTitle // This will tell us which answer the user choose true or false based upon the button title.
         
-        let actualAnswer = quiz [questionNumber][1] // This will match the user answer with the correct answer, The correct answer is the 1th element of array.
+        let actualAnswer = quiz[questionNumber].answer // This will match the user answer with the correct answer, The correct answer is the 1th element of array.
         
         if userAnswer == actualAnswer { // Here we are creating if else to check whether the user answer is right or wrong if user answer is right, It will print right else wrong!.
             
-            print("Right!")
+            sender.backgroundColor = UIColor.green // This will show user green color when answer is correct!
         } else {
-            print("Wrong!")
+            sender.backgroundColor = UIColor.red  // This will show user red color when answer is correct!.
         }
         
         if questionNumber + 1 < quiz.count { // quiz.count is the length of our array.
@@ -52,14 +63,23 @@ class ViewController: UIViewController {
             questionNumber = 0 // This will take us to the first question when the final question is answered. This is creating an endless loop of our questions and answers of course until the user gets bored:).
         }
         
+       Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false) // This code is showing color on the button depending upon whether it is right or wrong this code makes sure that it takes 0.2 second delays to show the color and when we go to the next question the color gets clear as we have written UIcolor.clear in func updateUI()
         
         
-        updateUI() // Here we are calling this function to update the UI when the question number goes to another question number or increases.
+        
+        // We have commented because we are using it in the above code updateUI() // Here we are calling this function to update the UI when the question number goes to another question number or increases.
     }
     
-    func updateUI(){ // function to update UI for new questions.
+    @objc func updateUI(){ // function to update UI for new questions.
         
-        questionLabel.text = quiz[questionNumber][0] // This code is connecting quiz and question number and also updating the question label based on the current question number.
+        questionLabel.text = quiz[questionNumber].text // This code is connecting quiz and question number and also updating the question label based on the current question number.
+        
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
+        
+        // progressBar.progress code is helping us managing our progress bar we have used float here. In the above code we are diving question number from the total number of elements in quiz array so that we can see the progress.
+        // We have used + 1 to see some progress in our progressbar when it is at the first question.
     
     }
     
